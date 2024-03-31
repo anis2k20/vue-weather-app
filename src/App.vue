2932b21c51c9fef26be297d0786d6455ccba5b53
Anis <anis2k20@gmail.com>
2024-03-31 20:45:31 +0600

    project setup and weather api connected

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import WeatherData from "@/components/WeatherData.vue";
+</script>
+
+<template>
+  <Suspense>
+    <WeatherData/>
+  </Suspense>
+</template>
---
+++ src/components/WeatherData.vue
@@ -0,0 +1,33 @@
+<script setup>
+import axios from 'axios'
+import {ref} from 'vue'
+const data = ref(null)
+const tempC = ref(null)
+const tempF = ref(null)
+const localTime = ref(null)
+const isDay = ref(null)
+const condition = ref(null)
+const conditionIcon = ref(null)
+
+  try{
+      const res = await axios.get('https://api.weatherapi.com/v1/current.json?key=9fcd76cb0e8d442e859110234243103&q=Dhaka')
+      data.value = await res.data
+    }
+  catch (e){
+      console.error(e)
+    }
+
+tempC.value = data.value.current.temp_c
+tempF.value = data.value.current.temp_f
+localTime.value = data.value.location.localtime
+isDay.value = data.value.current.is_day
+condition.value = data.value.current.condition.text
+conditionIcon.value = data.value.current.condition.icon
+
+</script>
+
+<template>
+  <pre>{{data}}</pre>
+  <h3>{{tempC}}</h3>
+
+</template>
