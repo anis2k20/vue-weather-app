9ce4c303de28a94f9f5be547c8337cde9ffb5d90
Anis <anis2k20@gmail.com>
2024-04-01 00:12:01 +0600

    UI Done

--- src/components/WeatherData.vue
+++ src/components/WeatherData.vue
@@ -1,5 +1,6 @@
 <script setup>
 import axios from 'axios'
+import { Icon } from '@iconify/vue';
 import {ref} from 'vue'
 const data = ref(null)
 const tempC = ref(null)
@@ -27,7 +28,126 @@ conditionIcon.value = data.value.current.condition.icon
 </script>
 
 <template>
-  <pre>{{data}}</pre>
-  <h3>{{tempC}}</h3>
+  <div class="bg-gray-100 h-screen w-full flex flex-col flex-wrap justify-center items-center gap-10 sm:gap-8">
+<!--top -->
+    <div class="">
+      <h1 class="text-3xl sm:7xl font-bold font-poppins bg-white shadow-md sm:px-8 p-4 rounded text-primary">Vue Weather App </h1>
+    </div>
 
+<!--middle -->
+    <div class="flex flex-wrap justify-center gap-4">
+<!--  Left -->
+      <div class="p-6 w-80 rounded-lg bg-primary space-y-6 px-8">
+        <h4 class="font-semibold text-white text-center ">Dhaka, Bangladesh</h4>
+
+        <div class="flex justify-center gap-3 items-center">
+          <h2 class="text-7xl font-bold text-white">{{tempC}}°</h2>
+          <div class="space-y-2">
+            <h4 class="font-semibold text-white bg-white/20 px-2 rounded cursor-pointer">C</h4>
+            <h4 class="font-semibold px-2 cursor-pointer text-gray-800/50  rounded">F</h4>
+          </div>
+        </div>
+
+        <div class="flex justify-center content-stretch items-center rounded gap-2 bg-white p-2">
+          <Icon icon="noto:sun-behind-small-cloud" class="text-4xl" />
+          <span class="text-xl">{{condition}}</span>
+        </div>
+
+      </div>
+<!--  Right -->
+      <div class="p-6 bg-secondary rounded-lg w-80 flex flex-col justify-center items-center">
+        <h2 class="text-7xl font-bold">09:23</h2>
+        <span class="text-2xl font-semibold">03-11-2024</span>
+      </div>
+
+    </div>
+
+<!--bottom -->
+    <div class="flex flex-wrap justify-center gap-4">
+      <div class="w-24 text-center shadow-xl border border-black/10 rounded-lg">
+        <div class="bg-primary p-1 text-center rounded-t-lg text-white">
+          <span>SAT. 20</span>
+        </div>
+       <div class="bg-white p-2 rounded-b-lg">
+         <div class="flex justify-center items-center gap-2">
+           <Icon icon="noto:sun-behind-small-cloud" class="text-xl"></Icon>
+           <span class="font-semibold text-xl">{{tempC}}</span>
+         </div>
+         <h6 class="text-xs font-medium">Mostly cloudy</h6>
+       </div>
+
+      </div>
+
+      <div class="w-24 text-center shadow-xl border border-black/10 rounded-lg">
+        <div class="bg-primary p-1 text-center rounded-t-lg text-white">
+          <span>SAT. 20</span>
+        </div>
+        <div class="bg-white p-2 rounded-b-lg">
+          <div class="flex justify-center items-center gap-2">
+            <Icon icon="noto:sun-behind-small-cloud" class="text-xl"></Icon>
+            <span class="font-semibold text-xl">{{tempC}}</span>
+          </div>
+          <h6 class="text-xs font-medium">Mostly cloudy</h6>
+        </div>
+
+      </div>
+
+      <div class="w-24 text-center shadow-xl border border-black/10 rounded-lg">
+        <div class="bg-primary p-1 text-center rounded-t-lg text-white">
+          <span>SAT. 20</span>
+        </div>
+        <div class="bg-white p-2 rounded-b-lg">
+          <div class="flex justify-center items-center gap-2">
+            <Icon icon="noto:sun-behind-small-cloud" class="text-xl"></Icon>
+            <span class="font-semibold text-xl">{{tempC}}</span>
+          </div>
+          <h6 class="text-xs font-medium">Mostly cloudy</h6>
+        </div>
+
+      </div>
+
+      <div class="w-24 text-center shadow-xl border border-black/10 rounded-lg">
+        <div class="bg-primary p-1 text-center rounded-t-lg text-white">
+          <span>SAT. 20</span>
+        </div>
+        <div class="bg-white p-2 rounded-b-lg">
+          <div class="flex justify-center items-center gap-2">
+            <Icon icon="noto:sun-behind-small-cloud" class="text-xl"></Icon>
+            <span class="font-semibold text-xl">{{tempC}}</span>
+          </div>
+          <h6 class="text-xs font-medium">Mostly cloudy</h6>
+        </div>
+
+      </div>
+
+      <div class="w-24 text-center shadow-xl border border-black/10 rounded-lg">
+        <div class="bg-primary p-1 text-center rounded-t-lg text-white">
+          <span>SAT. 20</span>
+        </div>
+        <div class="bg-white p-2 rounded-b-lg">
+          <div class="flex justify-center items-center gap-2">
+            <Icon icon="noto:sun-behind-small-cloud" class="text-xl"></Icon>
+            <span class="font-semibold text-xl">{{tempC}}</span>
+          </div>
+          <h6 class="text-xs font-medium">Mostly cloudy</h6>
+        </div>
+
+      </div>
+
+      <div class="w-24 text-center shadow-xl border border-black/10 rounded-lg">
+        <div class="bg-primary p-1 text-center rounded-t-lg text-white">
+          <span>SAT. 20</span>
+        </div>
+        <div class="bg-white p-2 rounded-b-lg">
+          <div class="flex justify-center items-center gap-2">
+            <Icon icon="noto:sun-behind-small-cloud" class="text-xl"></Icon>
+            <span class="font-semibold text-xl">{{tempC}}</span>
+          </div>
+          <h6 class="text-xs font-medium">Mostly cloudy</h6>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
 </template>
